0eea8505831e738f4d0b38066c8291367a0c8364
Enrique Gonzalez <enrique@saucelabs.com>
2016-11-15 14:19:36 -0800

    initial commit

---
+++ sauce_test.js
@@ -0,0 +1,24 @@
+var webdriver = require('selenium-webdriver'),
+    username = "enriqueci",
+    accessKey = "ebf6fd4b-d68e-4939-a307-7cabb0bd5053",
+    driver;
+
+driver = new webdriver.Builder().
+  withCapabilities({
+    'browserName': 'chrome',
+    'platform': 'Windows XP',
+    'version': '43.0',
+    'username': username,
+    'accessKey': accessKey
+  }).
+  usingServer("https://" + username + ":" + accessKey +
+              "@ondemand.saucelabs.com:443/wd/hub").
+  build();
+
+driver.get('http://saucelabs.com/test/guinea-pig');
+
+driver.getTitle().then(function (title) {
+    console.log("title is: " + title);
+});
+
+driver.quit();
